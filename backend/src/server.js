3cf393fcb1305080e698f101b64d15c1d48d2f5b
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-01-30 11:46:46 -0300

    env mobile updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ const http = require('http');
 const routes = require('./routes');
 const { setupWebsocket } = require('./websocket');
 
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express();
 const server = http.Server(app);
@@ -14,14 +14,13 @@ const server = http.Server(app);
 setupWebsocket(server);
 
 mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: true
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: true
 });
-
 
 app.use(cors());
 app.use(express.json());
-app.use(routes); 
+app.use(routes);
 
-server.listen(3333);
+server.listen(process.env.PORT || 3333);
